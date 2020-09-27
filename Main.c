8d7834cf556e650783acb2a413e767243b290939
Ameer <Ameer@DESKTOP-93CPLEQ>
2020-09-27 13:48:42 +0200

    Initial Commit

---
+++ Main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+		return 1;
+	printf("What is the length of your square matrix ?");
+	return 0;
+}
